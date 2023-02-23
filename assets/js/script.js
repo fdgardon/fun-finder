ea0832ada00be4a870746f3f742aef96854e4e5e
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-02-22 22:28:24 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ var wind = weatherResults.children[1].children[3];
 // function for clicks on search button
 function searchResults() {
   var apiUrlWeather =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityInput.value +
     "," +
     stateSearch.value +
